a3c3ff0b5b937d71483472afb54448beb38835ca
Evan Powell <evanpowell87@gmail.com>
2024-03-14 15:38:57 -0500

    refactor nav link styles

--- components/main-header/nav-link.jsx
+++ components/main-header/nav-link.jsx
@@ -2,14 +2,31 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useMemo } from "react";
 
 import styles from "./nav-link.module.css";
+
+function getDynamicClassNames(dynamicClassNamesObj) {
+  return Object.entries(dynamicClassNamesObj).reduce(
+    (acc, [className, isTrue]) => {
+      return isTrue ? `${acc} ${className}` : acc;
+    },
+    ""
+  );
+}
 
 export default function NavLink({ href, children }) {
   const path = usePathname();
 
+  const linkClassNames = useMemo(() => {
+    return getDynamicClassNames({
+      [styles.link]: true,
+      [styles.active]: path === href,
+    });
+  }, [path]);
+
   return (
-    <Link href={href} className={path === href ? styles.active : ""}>
+    <Link href={href} className={linkClassNames}>
       {children}
     </Link>
   );
