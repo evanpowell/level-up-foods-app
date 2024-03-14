c513e5143fa02069ee012acc3ff4c8720399cb0f
Evan Powell <evanpowell87@gmail.com>
2024-03-14 13:53:11 -0500

    style header

--- components/main-header.jsx
+++ components/main-header.jsx
@@ -2,14 +2,16 @@ import Link from "next/link";
 
 import LogoImg from "@/assets/logo.png";
 
+import styles from "./main-header.module.css";
+
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
+    <header className={styles.header}>
+      <Link className={styles.logo} href="/">
         <img src={LogoImg.src} alt="A plate with food on it" />
-        Level Up Foods
+        LevelUp Foods
       </Link>
-      <nav>
+      <nav className={styles.nav}>
         <ul>
           <li>
             <Link href="/meals">Meals</Link>
