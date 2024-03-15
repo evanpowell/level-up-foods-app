32c14d532c76c9e38b04104d5690a7a41c0f04dc
Evan Powell <evanpowell87@gmail.com>
2024-03-14 21:20:59 -0500

    populate meal details page

---
+++ app/meals/[mealSlug]/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage() {
+  return (
+    <main className="not-found">
+      <h1>Meal Not Found</h1>
+      <p>The meal you are looking for could not be found.</p>
+    </main>
+  );
+}
--- app/meals/[mealSlug]/page.jsx
+++ app/meals/[mealSlug]/page.jsx
@@ -1,3 +1,40 @@
-export default function MealDetailsPage() {
-  return <h1>Meal Details Page</h1>;
+import Image from "next/image";
+
+import styles from "./page.module.css";
+import { getMeal } from "@/lib/meals";
+import { notFound } from "next/navigation";
+
+export default function MealDetailsPage({ params }) {
+  const meal = getMeal(params.mealSlug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  const mealInstructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: mealInstructions,
+          }}
+        ></p>
+      </main>
+    </>
+  );
 }
